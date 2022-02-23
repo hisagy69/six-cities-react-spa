15d5e5d67e90dfb8deff3284cac46e493c559876
hisagy69 <sinegamy11@gmail.com>
2022-02-23 22:34:18 +0700

    refactoring spinner

--- src/components/spinner.jsx
+++ src/components/spinner.jsx
@@ -1,18 +1,17 @@
-import React, {useRef} from 'react';
+import React, {useRef, useEffect} from 'react';
 const Spinner = () => {
   const spinnerRef = useRef();
   let i = 0;
   let key;
   const animate = () => {
-    if (!spinnerRef.current) {
-      cancelAnimationFrame(key);
-      return;
-    }
     i += 5;
     spinnerRef.current.style.transform = `rotate(${i}deg)`;
     key = requestAnimationFrame(animate);
   };
-  key = requestAnimationFrame(animate);
+  useEffect(() => {
+    key = requestAnimationFrame(animate);
+    cancelAnimationFrame(key);
+  }, []);
   return <div className="spinner" style={{position: `absolute`, left: `50%`, top: `50%`, transform: `translate(-50%, -50%)`}}>
     <img ref={spinnerRef} src="img/spinner.png" width="70"/>
   </div>;
